da581c4c5ee800c4421605d8438617d0cf7228b0
filip_neven <filip_neven@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2006-07-03 20:49:42 +0000

    Enabled automatic mock injection on unit class using the @Mock annotation Moved EasyMockTestCase to the src tree (instead of test tree)

--- trunk/src/be/ordina/unitils/testing/mock/EasyMockTestCase.java
+++ trunk/src/be/ordina/unitils/testing/mock/EasyMockTestCase.java
@@ -11,20 +11,29 @@ import org.easymock.classextension.internal.ClassExtensionHelper;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.lang.reflect.Field;
 
 import static org.easymock.classextension.EasyMock.*;
 
 /**
- * @author Filip Neven
+ * Base class for testing with mock objects using EasyMock.
  */
 public class EasyMockTestCase extends TestCase {
 
     private Set mocks = new HashSet();
 
+    protected void setUp() throws Exception {
+        injectMocksIntoTest();
+    }
+
     protected <T> T getMock(Class<T> clazz) {
-        T result = createMock(clazz);
-        registerMock(result);
-        return result;
+        T mock = createMock(clazz);
+        registerMock(mock);
+        injectMock(clazz, mock);
+        return mock;
+    }
+
+    protected void injectMock(Class clazz, Object mock) {
     }
 
     private void registerMock(Object mock) {
@@ -40,6 +49,17 @@ public class EasyMockTestCase extends TestCase {
     protected void verify() {
         for (Object mock : mocks) {
             ClassExtensionHelper.getControl(mock).verify();
+        }
+    }
+
+    private void injectMocksIntoTest() throws IllegalAccessException {
+        Field[] fields = this.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            if (field.getAnnotation(Mock.class) != null) {
+                field.setAccessible(true);
+                Object mock = getMock(field.getType());
+                field.set(this, mock);
+            }
         }
     }
 
