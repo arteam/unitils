9c9db0146a80b502823e188c6a090ac0c87a72d9
filip_neven <filip_neven@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2006-10-27 18:02:25 +0000

    Finished implementation of HsqldbSequenceUpdater and written unit test

---
+++ src/test/java/org/unitils/dbmaintainer/sequences/HsqldbSequenceUpdaterTest.java
@@ -0,0 +1,28 @@
+package org.unitils.dbmaintainer.sequences;
+
+import org.apache.commons.dbutils.DbUtils;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ */
+public class HsqldbSequenceUpdaterTest extends SequenceUpdaterTest {
+
+    protected long getNextSequenceValue(Connection conn) throws SQLException {
+        Statement st = null;
+        ResultSet rset = null;
+        try {
+            st = conn.createStatement();
+            rset = st.executeQuery("select next value for testsequence from testtable");
+            rset.next();
+            long sequenceValue = rset.getLong(1);
+            return sequenceValue;
+        } finally {
+            DbUtils.closeQuietly(null, st, rset);
+        }
+    }
+}
