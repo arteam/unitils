cb2589ff0567935b5d4d711dd68eaa3c221f462f
filip_neven <filip_neven@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2006-12-28 23:12:21 +0000

    Added support for injection of a hibernate SessionFactory into the test object using the HibernateSessionFactory annotation

---
+++ branches/1.0/src/main/java/org/unitils/hibernate/annotation/HibernateSessionFactory.java
@@ -0,0 +1,19 @@
+package org.unitils.hibernate.annotation;
+
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Annotation indicating that this field or method should be initialized with the Hibernate<code>SessionFactory</code> object
+ * that can be used to create Hibernate <code>Session</code> object that provide a connection to the unit test database.
+ * If a field is annotated, it should be of type <code>org.hibernate.SessionFactory</code>. If a method is annotated,
+ * the method should have following signature: void myMethod(org.hibernate.SessionFactory sessionFactory)
+ *
+ * @author Filip Neven
+ */
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface HibernateSessionFactory {
+}
