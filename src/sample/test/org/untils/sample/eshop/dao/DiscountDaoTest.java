a10e3c20541c5558b37697644f77fe5f9df89aa2
filip_neven <filip_neven@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2006-12-29 17:57:28 +0000

    Added spring configuration for sample. Made sample dao's use spring's HibernateDaoSupport. Used the new feature of injection of the hibernate SessionFactory make sure dao's use the Unitils-provided DataSource

--- src/sample/test/org/untils/sample/eshop/dao/DiscountDaoTest.java
+++ src/sample/test/org/untils/sample/eshop/dao/DiscountDaoTest.java
@@ -3,12 +3,14 @@ package org.untils.sample.eshop.dao;
 import org.untils.sample.eshop.BaseHibernateDaoTest;
 import org.unitils.sample.eshop.dao.DiscountDao;
 import org.unitils.sample.eshop.model.User;
+import org.unitils.inject.annotation.TestedObject;
 
 /**
  * 
  */
 public class DiscountDaoTest extends BaseHibernateDaoTest {
 
+    @TestedObject
     private DiscountDao discountDao = new DiscountDao();
 
     public void testCalculateTotalPurchaseAmount() {
