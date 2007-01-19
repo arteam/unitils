a8401531222ad4c96e09c395065480986cba7ecf
tim_ducheyne <tim_ducheyne@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2007-01-19 13:53:54 +0000

    DataSet and ExpectedDataSet: made inheritable

--- src/main/java/org/unitils/dbunit/annotation/ExpectedDataSet.java
+++ src/main/java/org/unitils/dbunit/annotation/ExpectedDataSet.java
@@ -17,6 +17,7 @@ package org.unitils.dbunit.annotation;
 
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Target;
@@ -71,6 +72,7 @@ import java.lang.annotation.Target;
  */
 @Target({TYPE, METHOD})
 @Retention(RUNTIME)
+@Inherited
 public @interface ExpectedDataSet {
 
 
