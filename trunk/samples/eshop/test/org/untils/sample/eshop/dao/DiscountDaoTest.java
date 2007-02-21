dd5998141789d7694df3de95892f6962c0c70611
filip_neven <filip_neven@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2007-02-21 21:19:12 +0000

    Updated sample

--- trunk/samples/eshop/test/org/untils/sample/eshop/dao/DiscountDaoTest.java
+++ trunk/samples/eshop/test/org/untils/sample/eshop/dao/DiscountDaoTest.java
@@ -1,17 +1,16 @@
 package org.untils.sample.eshop.dao;
 
-import org.untils.sample.eshop.BaseHibernateDaoTest;
 import org.unitils.sample.eshop.dao.DiscountDao;
 import org.unitils.sample.eshop.model.User;
-import org.unitils.inject.annotation.TestedObject;
 import org.unitils.dbunit.annotation.DataSet;
 import org.unitils.spring.annotation.SpringBeanByType;
+import org.untils.sample.eshop.BaseHibernateTest;
 
 /**
  * 
  */
 @DataSet
-public class DiscountDaoTest extends BaseHibernateDaoTest {
+public class DiscountDaoTest extends BaseHibernateTest {
 
     @SpringBeanByType
     private DiscountDao discountDao = new DiscountDao();
