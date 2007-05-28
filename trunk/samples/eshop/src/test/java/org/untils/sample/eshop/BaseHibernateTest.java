e9de6b546a8561f26b889d919c8b2cb66340f133
tim_ducheyne <tim_ducheyne@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2007-05-28 08:50:48 +0000

    Upgraded to rc3

--- trunk/samples/eshop/src/test/java/org/untils/sample/eshop/BaseHibernateTest.java
+++ trunk/samples/eshop/src/test/java/org/untils/sample/eshop/BaseHibernateTest.java
@@ -1,19 +1,15 @@
 package org.untils.sample.eshop;
 
-import org.unitils.spring.annotation.SpringApplicationContext;
-import org.hibernate.cfg.Configuration;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.orm.hibernate3.LocalSessionFactoryBean;
 import org.unitils.UnitilsJUnit3;
-import org.unitils.hibernate.annotation.HibernateSessionFactory;
+import org.unitils.spring.annotation.SpringApplicationContext;
 
 /**
  * @author Filip Neven
  * @author Tim Ducheyne
  */
 @SpringApplicationContext("classpath:/eshop-config.xml")
-public class BaseHibernateTest extends UnitilsJUnit3 {
+public abstract class BaseHibernateTest extends UnitilsJUnit3 {
 
     @SpringApplicationContext
     ApplicationContext springApplicationContext;
