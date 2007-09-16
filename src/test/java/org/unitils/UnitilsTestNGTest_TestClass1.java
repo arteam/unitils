f2b92bac618de2f0ac2d694909a0872149e3a585
tim_ducheyne <tim_ducheyne@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2007-09-16 09:09:00 +0000

    Changed copyright to unitils.org

--- src/test/java/org/unitils/UnitilsTestNGTest_TestClass1.java
+++ src/test/java/org/unitils/UnitilsTestNGTest_TestClass1.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006 the original author or authors.
+ * Copyright 2006-2007,  Unitils.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
