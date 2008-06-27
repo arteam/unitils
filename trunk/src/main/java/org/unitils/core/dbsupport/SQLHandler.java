b1546ed41721e273e7968972bed308b3d1b6d402
filip_neven <filip_neven@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2008-06-27 07:04:16 +0000

    Added missing class to fix build

---
+++ trunk/src/main/java/org/unitils/core/dbsupport/SQLHandler.java
@@ -0,0 +1,71 @@
+package org.unitils.core.dbsupport;
+
+import java.util.Set;
+
+import javax.sql.DataSource;
+
+import org.unitils.core.UnitilsException;
+
+public interface SQLHandler {
+
+	/**
+	 * Executes the given statement.
+	 *
+	 * @param sql The sql string for retrieving the items
+	 * @return The nr of updates
+	 */
+	int executeUpdate(String sql);
+
+	/**
+	 * Executes the given code update statement.
+	 *
+	 * @param sql The sql string for retrieving the items
+	 * @return The nr of updates
+	 */
+	int executeCodeUpdate(String sql);
+
+	/**
+	 * Returns the long extracted from the result of the given query. If no value is found, a {@link UnitilsException}
+	 * is thrown.
+	 *
+	 * @param sql The sql string for retrieving the items
+	 * @return The long item value
+	 */
+	long getItemAsLong(String sql);
+
+	/**
+	 * Returns the value extracted from the result of the given query. If no value is found, a {@link UnitilsException}
+	 * is thrown.
+	 *
+	 * @param sql The sql string for retrieving the items
+	 * @return The string item value
+	 */
+	String getItemAsString(String sql);
+
+	/**
+	 * Returns the items extracted from the result of the given query.
+	 *
+	 * @param sql The sql string for retrieving the items
+	 * @return The items, not null
+	 */
+	Set<String> getItemsAsStringSet(String sql);
+
+	/**
+	 * Returns true if the query returned a record.
+	 *
+	 * @param sql The sql string for checking the existence
+	 * @return True if a record was returned
+	 */
+	boolean exists(String sql);
+
+	/**
+	 * @return The DataSource
+	 */
+	DataSource getDataSource();
+
+	/**
+	 * @return Whether updates are executed on the database or not
+	 */
+	boolean isDoExecuteUpdates();
+
+}
