2fc2c3e05f3b4db2a60681e5ad1c35599a821216
tim_ducheyne <tim_ducheyne@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2009-05-27 20:15:32 +0000

    UNI-143: auto unwrap mock

--- src/test/java/org/unitils/mock/core/MockObjectTest.java
+++ src/test/java/org/unitils/mock/core/MockObjectTest.java
@@ -16,7 +16,6 @@
 package org.unitils.mock.core;
 
 import static junit.framework.Assert.*;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,12 +23,9 @@ import org.unitils.core.util.CloneUtil;
 import static org.unitils.mock.ArgumentMatchers.notNull;
 import org.unitils.mock.mockbehavior.MockBehavior;
 import org.unitils.mock.proxy.ProxyInvocation;
-import static org.unitils.reflectionassert.ReflectionAssert.assertLenientEquals;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * Tests the mock object functionality.
@@ -43,6 +39,7 @@ public class MockObjectTest {
     private MockObject<TestClass> mockObject1, mockObject2;
     private MockObject<MockReturning> mockReturningOtherMock;
 
+
     @Before
     public void setUp() {
         Scenario scenario = new Scenario(null);
@@ -52,233 +49,51 @@ public class MockObjectTest {
     }
 
 
-    /**
-     * Tests setting a return behavior for the mock. The behavior is an always matching behavior
-     * so the method should keep returning that same value.
-     */
     @Test
-    public void testReturns() {
-        mockObject1.returns("aValue").testMethodString();
-
-        String result1 = mockObject1.getMock().testMethodString();
-        String result2 = mockObject1.getMock().testMethodString();
-        assertLenientEquals("aValue", result1);
-        assertLenientEquals("aValue", result2);
-    }
-
-
-    /**
-     * Tests setting a once return behavior for the mock. The behavior should be executed only once, the second time
-     * the default null value is returned.
-     */
-    @Test
-    public void testOnceReturns() {
-        mockObject1.onceReturns("aValue").testMethodString();
-
-        String result1 = mockObject1.getMock().testMethodString();
-        String result2 = mockObject1.getMock().testMethodString();
-        assertLenientEquals("aValue", result1);
-        assertNull(result2);
-    }
-
-
-    /**
-     * Tests the return behavior when no behavior was defined. The null value should be
-     * returned as default object value.
-     */
-    @Test
-    public void testReturns_defaultBehaviorObject() {
-        String result = mockObject1.getMock().testMethodString();
-        assertLenientEquals(null, result);
-    }
-
-
-    /**
-     * Tests the return behavior when no behavior was defined. The 0 value should be
-     * returned as default number value.
-     */
-    @Test
-    public void testReturns_defaultBehaviorNumber() {
-        int result = mockObject1.getMock().testMethodNumber();
-        assertLenientEquals(0, result);
-    }
-
-
-    /**
-     * Tests the return behavior when no behavior was defined. An empty list should be
-     * returned as default list value.
-     */
-    @Test
-    public void testReturns_defaultBehaviorList() {
-        List<String> result = mockObject1.getMock().testMethodList();
-        assertLenientEquals(0, result.size());
-    }
-
-
-    /**
-     * Tests the return behavior when no behavior was defined. An empty set should be
-     * returned as default set value.
-     */
-    @Test
-    public void testReturns_defaultBehaviorSet() {
-        Set<String> result = mockObject1.getMock().testMethodSet();
-        assertLenientEquals(0, result.size());
-    }
-
-
-    /**
-     * Tests the return behavior when no behavior was defined. An empty map should be
-     * returned as default map value.
-     */
-    @Test
-    public void testReturns_defaultBehaviorMap() {
-        Map<String, String> result = mockObject1.getMock().testMethodMap();
-        assertLenientEquals(0, result.size());
-    }
-
-
-    /**
-     * Tests setting an exception behavior for the mock. The behavior is an always matching behavior
-     * so the method should keep throwing that same exception.
-     */
-    @Test
-    public void testRaises() {
-        mockObject1.raises(new ThreadDeath()).testMethodString();
-
-        boolean exception1 = false;
-        try {
-            mockObject1.getMock().testMethodString();
-        } catch (ThreadDeath e) {
-            exception1 = true;
-        }
-        boolean exception2 = false;
-        try {
-            mockObject1.getMock().testMethodString();
-        } catch (ThreadDeath e) {
-            exception2 = true;
-        }
-        assertTrue(exception1);
-        assertTrue(exception2);
-    }
-
-
-    /**
-     * Tests setting an once exception behavior for the mock. The behavior should be executed only once, the second time
-     * no exception should be raised.
-     */
-    @Test
-    public void testOnceRaises() {
-        mockObject1.onceRaises(new ThreadDeath()).testMethodString();
-
-        boolean exception1 = false;
-        try {
-            mockObject1.getMock().testMethodString();
-        } catch (ThreadDeath e) {
-            exception1 = true;
-        }
-        boolean exception2 = false;
-        try {
-            mockObject1.getMock().testMethodString();
-        } catch (ThreadDeath e) {
-            exception2 = true;
-        }
-        assertTrue(exception1);
-        assertFalse(exception2);
-    }
-
-
-    @Test
-    public void testRaises_exceptionClass() {
-        mockObject1.raises(IllegalArgumentException.class).testMethodString();
-
-        try {
-            mockObject1.getMock().testMethodString();
-            fail("Expected exception");
-        } catch (IllegalArgumentException e) {
-            // expected
-        }
-    }
-
-
-    @Test
-    public void testOnceRaises_exceptionClass() {
-        mockObject1.onceRaises(IllegalArgumentException.class).testMethodString();
-
-        try {
-            mockObject1.getMock().testMethodString();
-            fail("Expected exception");
-        } catch (IllegalArgumentException e) {
-            // expected
-        }
-    }
-
-
-    /**
-     * Tests setting a peforms behavior for the mock. The behavior is an always matching behavior
-     * so the method should keep performing that same behavior.
-     */
-    @Test
-    public void testPerforms() {
-        TestMockBehavior testMockBehavior = new TestMockBehavior();
-        mockObject1.performs(testMockBehavior).testMethodString();
-
-        mockObject1.getMock().testMethodString();
-        mockObject1.getMock().testMethodString();
-        assertLenientEquals(2, testMockBehavior.invocationCount);
-    }
-
-
-    /**
-     * Tests setting a once peforms behavior for the mock. The behavior should be executed only once, the second time
-     * nothing should have happened.
-     */
-    @Test
-    public void testOncePerforms() {
-        TestMockBehavior testMockBehavior = new TestMockBehavior();
-        mockObject1.oncePerforms(testMockBehavior).testMethodString();
-
-        mockObject1.getMock().testMethodString();
-        mockObject1.getMock().testMethodString();
-        assertLenientEquals(1, testMockBehavior.invocationCount);
-    }
-
-
     @SuppressWarnings("unchecked")
-    @Test
     public void testInvocationSpreadOverMoreThanOneLine() {
         TestMockBehavior testMockBehavior = new TestMockBehavior();
-        mockObject1.performs(testMockBehavior).testMethodParam(
-                notNull(List.class));
+        mockObject1.performs(testMockBehavior).testMethodParam(notNull(List.class));
         mockObject1.getMock().testMethodParam(null);
         assertEquals(0, testMockBehavior.invocationCount);
         mockObject1.getMock().testMethodParam(new ArrayList<String>());
         assertEquals(1, testMockBehavior.invocationCount);
     }
 
+
     @Test
-    public void testCorrectAssertionFailedStackTrace() {
+    public void testAssertInvokedFailure() {
         try {
             mockObject1.assertInvoked().testMethodString();
             fail();
         } catch (AssertionError e) {
-            assertTopOfStackTracePointsToCurrentTest(e, "testCorrectAssertionFailedStackTrace");
+            assertTopOfStackTracePointsToCurrentTest(e, "testAssertInvokedFailure");
         }
+    }
 
+
+    @Test
+    public void testAssertNotInvokedFailure() {
         try {
             mockObject1.getMock().testMethodString();
             mockObject1.assertNotInvoked().testMethodString();
             fail();
         } catch (AssertionError e) {
-            assertTopOfStackTracePointsToCurrentTest(e, "testCorrectAssertionFailedStackTrace");
+            assertTopOfStackTracePointsToCurrentTest(e, "testAssertNotInvokedFailure");
         }
+    }
 
+
+    @Test
+    public void testAssertInvokedInSequenceFailure() {
         try {
             mockObject1.assertInvokedInSequence().testMethodArray();
             fail();
         } catch (AssertionError e) {
-            assertTopOfStackTracePointsToCurrentTest(e, "testCorrectAssertionFailedStackTrace");
+            assertTopOfStackTracePointsToCurrentTest(e, "testAssertInvokedInSequenceFailure");
         }
     }
+
 
     @Test
     public void testEquals() {
@@ -286,11 +101,18 @@ public class MockObjectTest {
         assertFalse(mockObject1.getMock().equals(mockObject2.getMock()));
     }
 
+
     @Test
-    public void testHashcode() {
+    public void testHashcode_equalObjects() {
         assertTrue(mockObject1.getMock().hashCode() == mockObject1.getMock().hashCode());
+    }
+
+
+    @Test
+    public void testHashcode_differentObjects() {
         assertFalse(mockObject1.getMock().hashCode() == mockObject2.getMock().hashCode());
     }
+
 
     @Test
     public void testDeepCloneEqualToOriginal() {
@@ -298,6 +120,7 @@ public class MockObjectTest {
         TestClass clone = CloneUtil.createDeepClone(mockObject1.getMock());
         assertEquals(mockObject1.getMock(), clone);
     }
+
 
     /**
      * Test written for bugfix: if a mocked method returns another mock object, this other mock object is cloned
@@ -316,11 +139,13 @@ public class MockObjectTest {
         }
     }
 
+
     private void assertTopOfStackTracePointsToCurrentTest(Throwable e, Object testMethodName) {
         StackTraceElement topOfStackTrace = e.getStackTrace()[0];
         assertEquals(MockObjectTest.class.getName(), topOfStackTrace.getClassName());
         assertEquals(testMethodName, topOfStackTrace.getMethodName());
     }
+
 
     /**
      * Interface that is mocked during the tests
@@ -328,14 +153,6 @@ public class MockObjectTest {
     private static interface TestClass {
 
         public String testMethodString();
-
-        public int testMethodNumber();
-
-        public List<String> testMethodList();
-
-        public Set<String> testMethodSet();
-
-        public Map<String, String> testMethodMap();
 
         public int[] testMethodArray();
 
