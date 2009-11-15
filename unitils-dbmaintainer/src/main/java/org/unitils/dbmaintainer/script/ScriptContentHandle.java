b31fb3499101bacfab7f57b6c0f34f45258daf61
tim_ducheyne <tim_ducheyne@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2009-11-15 15:58:04 +0000

    UNI-181: Remove hibernate dependency of dbmaintainer and make dbunit an optional dependency

--- unitils-dbmaintainer/src/main/java/org/unitils/dbmaintainer/script/ScriptContentHandle.java
+++ unitils-dbmaintainer/src/main/java/org/unitils/dbmaintainer/script/ScriptContentHandle.java
@@ -15,36 +15,32 @@
  */
 package org.unitils.dbmaintainer.script;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringReader;
+import org.unitils.core.UnitilsException;
+import org.unitils.thirdparty.org.apache.commons.io.IOUtils;
+import org.unitils.thirdparty.org.apache.commons.io.NullWriter;
+import org.unitils.util.ReaderInputStream;
+
+import java.io.*;
 import java.net.URL;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-import org.hibernate.lob.ReaderInputStream;
-import org.unitils.core.UnitilsException;
-import org.unitils.thirdparty.org.apache.commons.io.IOUtils;
-import org.unitils.thirdparty.org.apache.commons.io.NullWriter;
-
 /**
  * A handle for getting the script content as a stream.
- * 
+ *
  * @author Tim Ducheyne
  * @author Filip Neven
  */
 public abstract class ScriptContentHandle {
 
-	private MessageDigest scriptDigest;
-	
-	private Reader scriptReader;
-	
+    private MessageDigest scriptDigest;
+
+    private Reader scriptReader;
+
     /**
      * Opens a stream to the content of the script.
-     * 
+     *
      * NOTE: do not forget to close the stream after usage.
      *
      * @return The content stream, not null
@@ -52,50 +48,50 @@ public abstract class ScriptContentHandle {
     public Reader openScriptContentReader() {
         scriptDigest = getScriptDigest();
         scriptReader = new InputStreamReader(new DigestInputStream(getScriptInputStream(), scriptDigest));
-		return scriptReader;
+        return scriptReader;
     }
 
     protected MessageDigest getScriptDigest() {
-		try {
-			return MessageDigest.getInstance("MD5");
-		} catch (NoSuchAlgorithmException e) {
-			throw new UnitilsException(e);
-		}
-	}
-
-	public String getCheckSum() {
-		try {
-			if (scriptDigest == null) {
-				readScript();
-			} else if (scriptReader.ready()) {
-				throw new UnitilsException("Cannot obtain checksum, since a script is currently being read");
-			}
-			return getHexPresentation(scriptDigest.digest());
-		} catch (IOException e) {
-			throw new UnitilsException(e);
-		}
+        try {
+            return MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new UnitilsException(e);
+        }
     }
-	
-	
-	protected void readScript() throws IOException {
-		Reader scriptReader = openScriptContentReader();
-		IOUtils.copy(scriptReader, new NullWriter());
-		scriptReader.close();
-	}
 
-	
-	protected String getHexPresentation(byte[] byteArray) {
-		StringBuffer result = new StringBuffer();
-	    for (int i = 0; i < byteArray.length; i++) {
-	    	result.append(Integer.toString((byteArray[i] & 0xff) + 0x100, 16).substring(1));
-	    }
-	    return result.toString();
-	}
-    
+    public String getCheckSum() {
+        try {
+            if (scriptDigest == null) {
+                readScript();
+            } else if (scriptReader.ready()) {
+                throw new UnitilsException("Cannot obtain checksum, since a script is currently being read");
+            }
+            return getHexPresentation(scriptDigest.digest());
+        } catch (IOException e) {
+            throw new UnitilsException(e);
+        }
+    }
+
+
+    protected void readScript() throws IOException {
+        Reader scriptReader = openScriptContentReader();
+        IOUtils.copy(scriptReader, new NullWriter());
+        scriptReader.close();
+    }
+
+
+    protected String getHexPresentation(byte[] byteArray) {
+        StringBuffer result = new StringBuffer();
+        for (byte b : byteArray) {
+            result.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+        }
+        return result.toString();
+    }
+
 
     protected abstract InputStream getScriptInputStream();
 
-    
+
     /**
      * A handle for getting the script content as a stream.
      */
@@ -116,17 +112,17 @@ public abstract class ScriptContentHandle {
 
         /**
          * Opens a stream to the content of the script.
-         * 
+         *
          * @return The content stream, not null
          */
-		@Override
-		protected InputStream getScriptInputStream() {
-			try {
-				return url.openStream();
-			} catch (IOException e) {
+        @Override
+        protected InputStream getScriptInputStream() {
+            try {
+                return url.openStream();
+            } catch (IOException e) {
                 throw new UnitilsException("Error while trying to create reader for url " + url, e);
             }
-		}
+        }
     }
 
 
@@ -153,12 +149,12 @@ public abstract class ScriptContentHandle {
          *
          * @return The content stream, not null
          */
-		@Override
-		protected InputStream getScriptInputStream() {
-			return new ReaderInputStream(new StringReader(scriptContent));
-		}
-        
-        
+        @Override
+        protected InputStream getScriptInputStream() {
+            return new ReaderInputStream(new StringReader(scriptContent));
+        }
+
+
     }
 
 
