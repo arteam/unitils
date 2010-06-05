8b4bab18b3e21ecbd7fd4efdf5c3b4c9f15d4aa9
tim_ducheyne <tim_ducheyne@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2010-06-05 12:57:30 +0000

    Implementing DataSet module: javadoc

--- unitils-dataset/src/main/java/org/unitils/dataset/annotation/DataSetRefresh.java
+++ unitils-dataset/src/main/java/org/unitils/dataset/annotation/DataSetRefresh.java
@@ -27,48 +27,64 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Annotation indicating that a data set should be loaded before the test run.
- * <p/>
- * If a class is annotated, a test data set will be loaded before the execution of each of the test methods in
- * the class. A data set file name can explicitly be specified. If no such file name is specified, first a data set
- * named 'classname'.'testmethod'.xml will be tried, if no such file exists, 'classname'.xml will be tried. If that
- * file also doesn't exist, an exception will be thrown. File names that start with '/' are treated absolute. File names
- * that do not start with '/', are relative to the current class.
- * <p/>
- * A test method can also be annotated with DataSet in which case you specify the data set that needs to be loaded
- * before this test method is run. Again, a file name can explicitly be specified or if not specified, the default will
- * be used: first 'classname'.'methodname'.xml and if that file does not exist 'classname'.xml.
- * <p/>
+ * Annotation for triggering the loading of a data set before the execution of a test.
+ * <br/><br/>
+ * Refresh means that if a record with the same primary key values is in the database, it will be updated. If no such record
+ * exists yet in the database, it will be inserted. If no or not all values for the PK columns are provided in the data set,
+ * an exception will be raised.
+ * See {@link org.unitils.dataset.annotation.DataSetCleanInsert} and {@link org.unitils.dataset.annotation.DataSetInsert}
+ * for other possible ways of loading.
+ * <br/><br/>
+ * This annotation can be put on method or on class level. If the annotation is found on class and method level, the
+ * one on method level is taken. If a method is annotated, a data set will be loaded when that test method is run.
+ * If a class (or super-class) is annotated, a data set will be loaded for all test methods in that class.
+ * <br/><br/>
+ * You can explicitly specify 1 or more file names of data sets that need to be loaded. If no file name is specified,
+ * a default file name, 'classname'.xml, will be used. File names that start with '/' are treated absolute. File names
+ * that do not start with '/', are treated relative to the current class. See {@link org.unitils.dataset.resolver.impl.DefaultDataSetResolver}
+ * for more info and more options.
+ * <br/><br/>
+ * Variables can be provided. The first variable will replace all $1 values in the data set, the second variable all $2 values
+ * and so on. If no variable is provided for a place holder, it will not be replaced.
+ * <br/><br/>
+ * When you know that a test will not modify any data in the database (eg only queries data), you can mark a data set as
+ * read-only. If a next test needs to load the same data set (and there was no other non-read-only test) it will not
+ * be loaded again. This will improve the performance of the tests, avoiding unnecessary data set loading.
+ * <br/><br/>
  * Examples:
  * <pre><code>
- * '    @DataSet
- *      public class MyTestClass extends UnitilsJUnit3 {
+ * '    @DataSetRefresh
+ * '    public class MyTestClass extends UnitilsJUnit4 {
  * '
- *          public void testMethod1(){
- *          }
+ * '        @Test
+ * '        public void method1(){
+ * '        }
  * '
- * '        @DataSet("aCustomFileName.xml")
- *          public void testMethod2(){
- *          }
- *      }
+ * '        @Test
+ * '        @DataSetRefresh("aCustomFileName.xml")
+ * '        public void method2(){
+ * '        }
+ * '    }
  * </code></pre>
- * Will load a data set file named MyTestClass.xml or MyTestClass-testMethod1.xml for testMethod1 in the same directory
- * as the class. And for testMethod2 a data set file named aCustomFileName.xml in the same directory as the class is
+ * Will load a data set file named MyTestClass.xml in the same directory as the class for method1 and
+ * a data set file named aCustomFileName.xml in the same directory as the class for method2.
  * loaded.
- * <p/>
+ * <br/><br/>
  * <pre><code>
- *      public class MyTestClass extends UnitilsJUnit3 {
+ * '    public class MyTestClass extends UnitilsJUnit4 {
  * '
- *          public void testMethod1(){
- *          }
+ * '        @Test
+ * '        public void method1(){
+ * '        }
  * '
- * '        @DataSet
- *          public void testMethod2(){
- *          }
- *      }
+ * '        @Test
+ * '        @DataSetRefresh
+ * '        public void method2(){
+ * '        }
+ * '    }
  * </code></pre>
- * Will not load any data set for testMethod1 (there is no class level data set). Will load a data set file named
- * MyTestClass.xml or MyTestClass.testMethod2.xml for testMethod2.
+ * Will not load any data set for method1 (there is no class level data set) and will load a data set file named
+ * MyTestClass.xml for method2.
  *
  * @author Tim Ducheyne
  * @author Filip Neven
@@ -80,9 +96,8 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 public @interface DataSetRefresh {
 
     /**
-     * The file name of the data set. If left empty, the default filename will
-     * be used: first 'classname'.'testMethodname'.xml will be tried, if that file does not exist,
-     * 'classname'.xml is tried. If that file also does not exist, an exception is thrown.
+     * The file names of the data sets. If left empty, the default filename will
+     * be used: 'classname'.xml. An UnitilsException will be raised when a data set file cannot be found.
      *
      * @return the fileName, empty for default
      */
@@ -90,16 +105,16 @@ public @interface DataSetRefresh {
 
     /**
      * Variables that will be used to fill in possible variable declarations in a data set.
-     *
+     * <p/>
      * A data set can contain variable declarations: $0, $1 ...
      * When the data set is loaded, these declarations are replaced with the given variable values.
      * The first variable will replace $0, the second $1 ...
      * When no value is found, the $0 variable declaration will not be replaced and will remain in the data set.
-     *
+     * <p/>
      * The character that defines a variable token, e.g. $0, can be defined using the variableToken parameter on the
      * dataset root xml element. By default $ is used, but you can override this default by setting
      * the dataset.variabletoken.default unitils property.
-     *
+     * <p/>
      * Variable declarations can be escaped by using a double variable token, e.g. $$0 will be replaced by $0
      * instead of the variable value
      *
@@ -107,5 +122,12 @@ public @interface DataSetRefresh {
      */
     String[] variables() default {};
 
+    /**
+     * You can mark a data set as read-only if you're sure that the tests will not modify the data. If a next test needs
+     * the same data set to be loaded, it will not be loaded again avoiding unnecessary data set loading.
+     *
+     * @return True if the data of the data sets will not be modified by the tests, false by default
+     */
     boolean readOnly() default false;
+
 }
