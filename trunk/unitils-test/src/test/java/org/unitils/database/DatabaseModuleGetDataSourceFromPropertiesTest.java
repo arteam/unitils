a8a366f33ce7d6fad87565e3da10f8a3351d76d0
tim_ducheyne <tim_ducheyne@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2010-10-17 19:54:52 +0000

    Refactoring database module

--- trunk/unitils-test/src/test/java/org/unitils/database/DatabaseModuleGetDataSourceFromPropertiesTest.java
+++ trunk/unitils-test/src/test/java/org/unitils/database/DatabaseModuleGetDataSourceFromPropertiesTest.java
@@ -15,18 +15,18 @@
  */
 package org.unitils.database;
 
-import org.dbmaintain.database.DatabaseException;
 import org.junit.Before;
 import org.junit.Test;
 import org.unitils.UnitilsJUnit4;
 import org.unitils.core.ConfigurationLoader;
+import org.unitils.core.UnitilsException;
 
 import javax.sql.DataSource;
 import java.util.Properties;
 
 import static org.dbmaintain.config.DbMaintainProperties.*;
 import static org.junit.Assert.assertNotNull;
-import static org.unitils.database.DatabaseModule.PROPERTY_UPDATEDATABASESCHEMA_ENABLED;
+import static org.unitils.database.transaction.UnitilsDatabaseManager.PROPERTY_UPDATEDATABASESCHEMA_ENABLED;
 
 /**
  * Tests for the DatabaseModule
@@ -46,63 +46,43 @@ public class DatabaseModuleGetDataSourceFromPropertiesTest extends UnitilsJUnit4
     public void setUp() throws Exception {
         configuration = new ConfigurationLoader().loadConfiguration();
         configuration.setProperty(PROPERTY_UPDATEDATABASESCHEMA_ENABLED, "false");
-
     }
 
 
     @Test
     public void getDefaultDatabase() throws Exception {
-        addConfigForDefaultDatabase(configuration);
         databaseModule.init(configuration);
 
         DataSource dataSource = databaseModule.getDataSource(null, null);
         assertNotNull(dataSource);
     }
 
-    @Test(expected = DatabaseException.class)
+    @Test(expected = UnitilsException.class)
     public void noDefaultDatabaseConfigured() throws Exception {
-        clearConfigForDefaultDatabase(configuration);
+        clearConfigForDefaultDatabase();
         databaseModule.init(configuration);
 
-        DataSource dataSource = databaseModule.getDataSource(null, null);
-        assertNotNull(dataSource);
+        databaseModule.getDataSource(null, null);
     }
 
     @Test
     public void namedDatabase() throws Exception {
-        addConfigForDatabase("myDatabase", configuration);
+        configuration.setProperty(PROPERTY_DATABASE_NAMES, "myDatabase");
         databaseModule.init(configuration);
 
         DataSource dataSource = databaseModule.getDataSource("myDatabase", null);
         assertNotNull(dataSource);
     }
 
-    @Test(expected = DatabaseException.class)
+    @Test(expected = UnitilsException.class)
     public void unknownDatabaseName() throws Exception {
-        addConfigForDatabase("myDatabase", configuration);
         databaseModule.init(configuration);
 
         DataSource dataSource = databaseModule.getDataSource("xxxx", null);
         assertNotNull(dataSource);
     }
 
-
-    private void addConfigForDefaultDatabase(Properties configuration) {
-        configuration.setProperty(PROPERTY_DRIVERCLASSNAME, "org.hsqldb.jdbcDriver");
-        configuration.setProperty(PROPERTY_URL, "jdbc:hsqldb:mem:unitils");
-        configuration.setProperty(PROPERTY_USERNAME, "sa");
-        configuration.setProperty(PROPERTY_PASSWORD, "");
-    }
-
-    private void addConfigForDatabase(String databaseName, Properties configuration) {
-        configuration.setProperty(PROPERTY_DATABASE_NAMES, databaseName);
-        configuration.setProperty(PROPERTY_DATABASE_START + '.' + databaseName + '.' + PROPERTY_DRIVERCLASSNAME_END, "org.hsqldb.jdbcDriver");
-        configuration.setProperty(PROPERTY_DATABASE_START + '.' + databaseName + '.' + PROPERTY_URL_END, "jdbc:hsqldb:mem:unitils");
-        configuration.setProperty(PROPERTY_DATABASE_START + '.' + databaseName + '.' + PROPERTY_USERNAME_END, "sa");
-        configuration.setProperty(PROPERTY_DATABASE_START + '.' + databaseName + '.' + PROPERTY_PASSWORD_END, "");
-    }
-
-    private void clearConfigForDefaultDatabase(Properties configuration) {
+    private void clearConfigForDefaultDatabase() {
         configuration.remove(PROPERTY_DRIVERCLASSNAME);
         configuration.remove(PROPERTY_URL);
         configuration.remove(PROPERTY_USERNAME);
