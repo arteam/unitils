aaafb719f995588779f5861b605b15b8500cb715
tim_ducheyne <tim_ducheyne@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2011-10-02 10:34:00 +0000

    Refactoring core module system: adding new UnitilsConfiguration implementation

---
+++ unitils-test/src/test/java/org/unitils/core/config/UnitilsConfigurationContainsPropertyTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2011, Unitils.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.unitils.core.config;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Tim Ducheyne
+ * @author Filip Neven
+ */
+public class UnitilsConfigurationContainsPropertyTest {
+
+    /* Tested object */
+    private UnitilsConfiguration unitilsConfiguration;
+
+
+    @Before
+    public void initialize() throws Exception {
+        Properties properties = new Properties();
+        properties.setProperty("property", "value");
+        unitilsConfiguration = new UnitilsConfiguration(properties);
+    }
+
+
+    @Test
+    public void found() {
+        boolean result = unitilsConfiguration.containsProperty("property");
+        assertTrue(result);
+    }
+
+    @Test
+    public void notFound() {
+        boolean result = unitilsConfiguration.containsProperty("xxx");
+        assertFalse(result);
+    }
+
+}
