2e7787107b0589fcae63ce73103f8f10d0a5c5e2
thomasderycke <thomasderycke@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2011-12-12 23:10:59 +0000

    - Bugfix dropdown hides after code - Improvement: dropdown now on click

--- site/3.2/src/site/resources/v1/js/menu.js
+++ site/3.2/src/site/resources/v1/js/menu.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
-	$("ul.children").parent().append("<span></span>"); //Only shows drop down trigger when js is enabled - Adds empty span tag after ul.children
-	$("ul.topnav li span").click(function() { //When trigger is clicked...
+	$("ul.topnav li").click(function() { //When trigger is clicked...
 		//Following events are applied to the children itself (moving children up and down)
-		$(this).parent().find("ul.children").slideDown('fast').show(); //Drop down the children on click
-		$(this).parent().hover(function() {
+		$(this).find("ul.children").slideDown('fast').show(); //Drop down the children on click
+		$(this).hover(function() {
 		}, function(){	
 			$(this).parent().find("ul.children").slideUp('slow'); //When the mouse hovers out of the children, move it back up
 		});
