0e5c4890ecf27b5b01b70b5dfc3a2a3698ca609a
tim_ducheyne <tim_ducheyne@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2012-03-12 19:37:57 +0000

    New core. Moved test-ng back to core and made junit and test-ng provided dependencies

---
+++ unitils-core/src/main/java/org/unitilsnew/UnitilsJUnit3.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2012,  Unitils.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.unitilsnew;
+
+import junit.framework.TestCase;
+import org.apache.commons.lang.StringUtils;
+import org.unitils.core.UnitilsException;
+import org.unitilsnew.core.Context;
+import org.unitilsnew.core.config.BootstrapContextFactory;
+import org.unitilsnew.core.listener.impl.UnitilsTestListener;
+
+import java.lang.reflect.Method;
+
+/**
+ * Base test class that will Unitils-enable your test. This base class will make sure that the
+ * core unitils test listener methods are invoked in the expected order. See {@link org.unitils.core.TestListener} for
+ * more information on the listener invocation order.
+ *
+ * @author Tim Ducheyne
+ * @author Filip Neven
+ */
+public abstract class UnitilsJUnit3 extends TestCase {
+
+    /* Keeps track of the test class for which tests are currently being executed. */
+    protected static Class<?> currentTestClass;
+
+    protected UnitilsTestListener unitilsTestListener;
+
+
+    /**
+     * Creates a test without a name. Be sure to call {@link junit.framework.TestCase#setName} afterwards.
+     */
+    public UnitilsJUnit3() {
+        this(null);
+    }
+
+
+    /**
+     * Creates a test with the given name. The name should be the name of the test method.
+     *
+     * @param name the name of the test method
+     */
+    public UnitilsJUnit3(String name) {
+        super(name);
+    }
+
+
+    /**
+     * Overridden JUnit3 method to be able to call {@link org.unitils.core.TestListener#beforeTestSetUp} and {@link org.unitils.core.TestListener#afterTestTearDown}.
+     *
+     * @throws Throwable If an error occurs during the test
+     */
+    @Override
+    public void runBare() throws Throwable {
+        if (!getClass().equals(currentTestClass)) {
+            currentTestClass = getClass();
+            getUnitilsTestListener().beforeTestClass(getClass());
+        }
+
+        Throwable firstThrowable = null;
+        try {
+            getUnitilsTestListener().beforeTestSetUp(this, getCurrentTestMethod());
+            super.runBare();
+
+        } catch (Throwable t) {
+            // hold exception until later, first call afterTestTearDown
+            firstThrowable = t;
+        }
+
+        try {
+            getUnitilsTestListener().afterTestTearDown();
+
+        } catch (Throwable t) {
+            // first exception is typically the most meaningful, so ignore second exception
+            if (firstThrowable == null) {
+                firstThrowable = t;
+            }
+        }
+
+        // if there were exceptions, throw the first one
+        if (firstThrowable != null) {
+            throw firstThrowable;
+        }
+    }
+
+
+    /**
+     * Overridden JUnit3 method to be able to call {@link org.unitils.core.TestListener#beforeTestMethod} and
+     * {@link org.unitils.core.TestListener#afterTestMethod}.
+     *
+     * @throws Throwable If an error occurs during the test
+     */
+    @Override
+    protected void runTest() throws Throwable {
+        Throwable firstThrowable = null;
+        try {
+            getUnitilsTestListener().beforeTestMethod();
+            super.runTest();
+
+        } catch (Throwable t) {
+            // hold exception until later, first call afterTestMethod
+            firstThrowable = t;
+        }
+
+        try {
+            getUnitilsTestListener().afterTestMethod(firstThrowable);
+
+        } catch (Throwable t) {
+            // first exception is typically the most meaningful, so ignore second exception
+            if (firstThrowable == null) {
+                firstThrowable = t;
+            }
+        }
+
+        // if an exception occurred during beforeTestMethod, the test or afterTestMethod, throw it
+        if (firstThrowable != null) {
+            throw firstThrowable;
+        }
+    }
+
+
+    /**
+     * Gets the method that has the same name as the current test.
+     *
+     * @return the method, not null
+     * @throws UnitilsException if the method could not be found
+     */
+    protected Method getCurrentTestMethod() {
+        String testName = getName();
+        if (StringUtils.isEmpty(testName)) {
+            throw new UnitilsException("Unable to find current test method. No test name provided (null) for test. Test class: " + getClass());
+        }
+
+        try {
+            return getClass().getMethod(getName());
+
+        } catch (NoSuchMethodException e) {
+            throw new UnitilsException("Unable to find current test method. Test name: " + getName() + " , test class: " + getClass(), e);
+        }
+    }
+
+
+    /**
+     * @return The unitils test listener
+     */
+    protected UnitilsTestListener getUnitilsTestListener() {
+        if (unitilsTestListener == null) {
+            unitilsTestListener = createUnitilsTestListener();
+        }
+        return unitilsTestListener;
+    }
+
+    protected UnitilsTestListener createUnitilsTestListener() {
+        BootstrapContextFactory bootstrapContextFactory = new BootstrapContextFactory();
+        Context context = bootstrapContextFactory.create();
+        return context.getInstanceOfType(UnitilsTestListener.class);
+    }
+}
---
+++ unitils-core/src/main/java/org/unitilsnew/UnitilsTestNG.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2012,  Unitils.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.unitilsnew;
+
+import org.testng.IHookCallBack;
+import org.testng.IHookable;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.unitilsnew.core.Context;
+import org.unitilsnew.core.config.BootstrapContextFactory;
+import org.unitilsnew.core.listener.impl.UnitilsTestListener;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Base test class that will Unitils-enable your test. This base class will make sure that the
+ * core unitils test listener methods are invoked in the expected order. See {@link org.unitils.core.TestListener} for
+ * more information on the listener invocation order.
+ *
+ * @author Tim Ducheyne
+ * @author Filip Neven
+ */
+public abstract class UnitilsTestNG implements IHookable {
+
+    /* True if beforeTestSetUp was called */
+    protected boolean beforeTestSetUpCalled = false;
+
+    protected UnitilsTestListener unitilsTestListener;
+
+    /**
+     * Called before a test of a test class is run. This is where {@link org.unitils.core.TestListener#afterCreateTestObject(Object)} is called.
+     */
+    @BeforeClass(alwaysRun = true)
+    protected void unitilsBeforeClass() {
+        getUnitilsTestListener().beforeTestClass(this.getClass());
+    }
+
+
+    /**
+     * Called before all test setup. This is where {@link org.unitils.core.TestListener#beforeTestSetUp} is called.
+     *
+     * @param testMethod The test method, not null
+     */
+    @BeforeMethod(alwaysRun = true)
+    protected void unitilsBeforeTestSetUp(Method testMethod) {
+        beforeTestSetUpCalled = true;
+        getUnitilsTestListener().beforeTestSetUp(this, testMethod);
+    }
+
+
+    /**
+     * Called after all test tear down. This is where {@link org.unitils.core.TestListener#afterTestTearDown} is called.
+     * <p/>
+     * NOTE: alwaysRun is enabled to be sure that this method is called even when an exception occurs during
+     * {@link #unitilsBeforeTestSetUp}.
+     *
+     * @param testMethod The test method, not null
+     */
+    @AfterMethod(alwaysRun = true)
+    protected void unitilsAfterTestTearDown(Method testMethod) {
+        // alwaysRun is enabled, extra test to ensure that unitilsBeforeTestSetUp was called
+        if (beforeTestSetUpCalled) {
+            beforeTestSetUpCalled = false;
+            getUnitilsTestListener().afterTestTearDown();
+        }
+    }
+
+
+    /**
+     * Implementation of the hookable interface to be able to call {@link org.unitils.core.TestListener#beforeTestMethod} and
+     * {@link org.unitils.core.TestListener#afterTestMethod}.
+     *
+     * @param callBack   the TestNG test callback, not null
+     * @param testResult the TestNG test result, not null
+     */
+    public void run(IHookCallBack callBack, ITestResult testResult) {
+        Throwable beforeTestMethodException = null;
+        try {
+            getUnitilsTestListener().beforeTestMethod();
+
+        } catch (Throwable e) {
+            // hold exception until later, first call afterTestMethod
+            beforeTestMethodException = e;
+        }
+
+        Throwable testMethodException = null;
+        if (beforeTestMethodException == null) {
+            callBack.runTestMethod(testResult);
+
+            // Since TestNG calls the method using reflection, the exception is wrapped in an InvocationTargetException
+            testMethodException = testResult.getThrowable();
+            if (testMethodException != null && testMethodException instanceof InvocationTargetException) {
+                testMethodException = ((InvocationTargetException) testMethodException).getTargetException();
+            }
+        }
+
+        Throwable afterTestMethodException = null;
+        try {
+            Throwable testThrowable = beforeTestMethodException != null ? beforeTestMethodException : testMethodException;
+            getUnitilsTestListener().afterTestMethod(testThrowable);
+
+        } catch (Throwable e) {
+            afterTestMethodException = e;
+        }
+
+        // if there were exceptions, make sure the exception that occurred first is reported by TestNG
+        if (beforeTestMethodException != null) {
+            throwException(beforeTestMethodException);
+        } else {
+            // We don't throw the testMethodException, it is already registered by TestNG and will be reported to the user
+            if (testMethodException == null && afterTestMethodException != null) {
+                throwException(afterTestMethodException);
+            }
+        }
+    }
+
+
+    /**
+     * Throws an unchecked excepton for the given throwable.
+     *
+     * @param throwable The throwable, not null
+     */
+    protected void throwException(Throwable throwable) {
+        if (throwable instanceof RuntimeException) {
+            throw (RuntimeException) throwable;
+        } else if (throwable instanceof Error) {
+            throw (Error) throwable;
+        } else {
+            throw new RuntimeException(throwable);
+        }
+    }
+
+
+    /**
+     * @return The unitils test listener
+     */
+    protected UnitilsTestListener getUnitilsTestListener() {
+        if (unitilsTestListener == null) {
+            unitilsTestListener = createUnitilsTestListener();
+        }
+        return unitilsTestListener;
+    }
+
+    protected UnitilsTestListener createUnitilsTestListener() {
+        BootstrapContextFactory bootstrapContextFactory = new BootstrapContextFactory();
+        Context context = bootstrapContextFactory.create();
+        return context.getInstanceOfType(UnitilsTestListener.class);
+    }
+}
