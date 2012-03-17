72e23d5d70c42c571f5bc5794395c553c9c7f7db
tim_ducheyne <tim_ducheyne@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2012-03-17 19:23:31 +0000

    New core. Unit testing + refactoring

---
+++ unitils-test/src/test/java/org/unitilsnew/core/TestInstanceEqualsTest.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2012,  Unitils.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.unitilsnew.core;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Tim Ducheyne
+ */
+public class TestInstanceEqualsTest {
+
+    private TestClass testClass1;
+    private TestClass testClass2;
+    private Object testObject1;
+    private Object testObject2;
+    private Method testMethod1;
+    private Method testMethod2;
+
+
+    @Before
+    public void initialize() throws Exception {
+        testClass1 = new TestClass(MyClass1.class);
+        testClass2 = new TestClass(MyClass2.class);
+        testObject1 = new MyClass1();
+        testObject2 = new MyClass2();
+        testMethod1 = MyClass1.class.getDeclaredMethod("method1");
+        testMethod2 = MyClass2.class.getDeclaredMethod("method2");
+    }
+
+
+    @Test
+    public void equal() {
+        TestInstance testInstance1 = new TestInstance(testClass1, testObject1, testMethod1);
+        TestInstance testInstance2 = new TestInstance(testClass1, testObject1, testMethod1);
+
+        assertTrue(testInstance1.equals(testInstance2));
+        assertTrue(testInstance2.equals(testInstance1));
+    }
+
+    @Test
+    public void same() {
+        TestInstance testInstance = new TestInstance(testClass1, testObject1, testMethod1);
+
+        assertTrue(testInstance.equals(testInstance));
+    }
+
+    @Test
+    public void notEqualTestClass() {
+        TestInstance testInstance1 = new TestInstance(testClass1, testObject1, testMethod1);
+        TestInstance testInstance2 = new TestInstance(testClass2, testObject1, testMethod1);
+
+        assertFalse(testInstance1.equals(testInstance2));
+        assertFalse(testInstance2.equals(testInstance1));
+    }
+
+    @Test
+    public void notEqualTestObject() {
+        TestInstance testInstance1 = new TestInstance(testClass1, testObject1, testMethod1);
+        TestInstance testInstance2 = new TestInstance(testClass1, testObject2, testMethod1);
+
+        assertFalse(testInstance1.equals(testInstance2));
+        assertFalse(testInstance2.equals(testInstance1));
+    }
+
+    @Test
+    public void notEqualTestMethod() {
+        TestInstance testInstance1 = new TestInstance(testClass1, testObject1, testMethod1);
+        TestInstance testInstance2 = new TestInstance(testClass1, testObject1, testMethod2);
+
+        assertFalse(testInstance1.equals(testInstance2));
+        assertFalse(testInstance2.equals(testInstance1));
+    }
+
+    @Test
+    @SuppressWarnings("ObjectEqualsNull")
+    public void notEqualToNull() {
+        TestInstance testInstance = new TestInstance(testClass1, testObject1, testMethod1);
+
+        assertFalse(testInstance.equals(null));
+    }
+
+    @Test
+    @SuppressWarnings("EqualsBetweenInconvertibleTypes")
+    public void notEqualToOtherType() {
+        TestInstance testInstance = new TestInstance(testClass1, testObject1, testMethod1);
+
+        assertFalse(testInstance.equals("xxx"));
+    }
+
+    @Test
+    public void nullTestClasses() {
+        TestInstance testInstance1 = new TestInstance(null, testObject1, testMethod1);
+        TestInstance testInstance2 = new TestInstance(null, testObject1, testMethod1);
+
+        assertTrue(testInstance1.equals(testInstance2));
+    }
+
+    @Test
+    public void nullTestObjects() {
+        TestInstance testInstance1 = new TestInstance(testClass1, null, testMethod1);
+        TestInstance testInstance2 = new TestInstance(testClass1, null, testMethod1);
+
+        assertTrue(testInstance1.equals(testInstance2));
+    }
+
+    @Test
+    public void nullTestMethods() {
+        TestInstance testInstance1 = new TestInstance(testClass1, testObject1, null);
+        TestInstance testInstance2 = new TestInstance(testClass1, testObject1, null);
+
+        assertTrue(testInstance1.equals(testInstance2));
+    }
+
+    @Test
+    public void nullTestClassesTestObjectsAndTestMethods() {
+        TestInstance testInstance1 = new TestInstance(null, null, null);
+        TestInstance testInstance2 = new TestInstance(null, null, null);
+
+        assertTrue(testInstance1.equals(testInstance2));
+    }
+
+
+    private static class MyClass1 {
+
+        public void method1() {
+        }
+    }
+
+    private static class MyClass2 {
+
+        public void method2() {
+        }
+    }
+}
