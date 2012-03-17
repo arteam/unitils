6ba209b0a6edb348c1ad2d7cae512d2feed6b77b
tim_ducheyne <tim_ducheyne@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2012-03-17 19:23:31 +0000

    New core. Unit testing + refactoring

--- trunk/unitils-core/src/main/java/org/unitilsnew/core/TestInstance.java
+++ trunk/unitils-core/src/main/java/org/unitilsnew/core/TestInstance.java
@@ -132,4 +132,35 @@ public class TestInstance {
             testFields.add(testField);
         }
     }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        TestInstance that = (TestInstance) o;
+        if (testClass != null ? !testClass.equals(that.testClass) : that.testClass != null) {
+            return false;
+        }
+        if (testMethod != null ? !testMethod.equals(that.testMethod) : that.testMethod != null) {
+            return false;
+        }
+        if (testObject != null ? !testObject.equals(that.testObject) : that.testObject != null) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = testClass != null ? testClass.hashCode() : 0;
+        result = 31 * result + (testMethod != null ? testMethod.hashCode() : 0);
+        result = 31 * result + (testObject != null ? testObject.hashCode() : 0);
+        return result;
+    }
 }
