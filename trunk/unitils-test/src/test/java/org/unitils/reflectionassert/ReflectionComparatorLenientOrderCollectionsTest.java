61321b032dba7f81bc01ce2363a5c0d2e76202a8
tim_ducheyne <tim_ducheyne@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2013-07-10 16:13:59 +0000

    unitils-core: typo

--- trunk/unitils-test/src/test/java/org/unitils/reflectionassert/ReflectionComparatorLenientOrderCollectionsTest.java
+++ trunk/unitils-test/src/test/java/org/unitils/reflectionassert/ReflectionComparatorLenientOrderCollectionsTest.java
@@ -15,15 +15,15 @@
  */
 package org.unitils.reflectionassert;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import org.junit.Before;
 import org.junit.Test;
-import static org.unitils.reflectionassert.ReflectionComparatorFactory.createRefectionComparator;
-import static org.unitils.reflectionassert.ReflectionComparatorMode.LENIENT_ORDER;
 import org.unitils.reflectionassert.difference.UnorderedCollectionDifference;
 
 import static java.util.Arrays.binarySearch;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.unitils.reflectionassert.ReflectionComparatorFactory.createRefectionComparator;
+import static org.unitils.reflectionassert.ReflectionComparatorMode.LENIENT_ORDER;
 
 
 /**
@@ -49,7 +49,7 @@ public class ReflectionComparatorLenientOrderCollectionsTest {
      * Test for UNI-156: ReflectionAssert.assertReflectionEquals is leading to an endless loop
      */
     @Test
-    public void lenientOrderPeformance() {
+    public void lenientOrderPerformance() {
         String[] expected = {"1", "2", "3", "4", "17", "18", "19", "20", "22", "23", "50"};
         String[] actual = {"1", "3", "4", "2", "17", "18", "19", "20", "21", "22", "23"};
 
