b3a974ecfbacd86052b0e07bf6048e1114536901
willemijnw <willemijnw@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2014-02-13 14:41:41 +0000

    testen Statements Junit runner

---
+++ unitils-test/src/test/java/org/unitils/core/junit/BeforeTestMethodStatementTest.java
@@ -0,0 +1,89 @@
+package org.unitils.core.junit;
+
+import java.lang.reflect.Method;
+
+import org.easymock.classextension.EasyMock;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.model.Statement;
+import org.unitils.UnitilsJUnit4TestClassRunner;
+import org.unitils.core.TestListener;
+import org.unitils.easymock.EasyMockUnitils;
+import org.unitils.easymock.annotation.Mock;
+
+
+/**
+ * test {@link BeforeTestMethodStatement}.
+ * 
+ * @author Willemijn Wouters
+ * 
+ * @since 3.4.1
+ * 
+ */
+@RunWith(UnitilsJUnit4TestClassRunner.class)
+public class BeforeTestMethodStatementTest {
+
+    @Mock
+    private TestListener listener;
+
+    @Mock
+    private Statement statement;
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link org.unitils.core.junit.BeforeTestMethodStatement#evaluate()}.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testEvaluateExceptionOnListener() throws Throwable {
+        TestClass2 testObject = new TestClass2();
+        Method testMethod = TestClass2.class.getMethod("test1");
+        listener.beforeTestMethod(testObject, testMethod);
+        EasyMock.expectLastCall().andThrow(new NullPointerException());
+
+        EasyMockUnitils.replay();
+        new BeforeTestMethodStatement(listener, statement, testMethod, testObject).evaluate();
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testEvaluateExceptionOnStatement() throws Throwable {
+        TestClass2 testObject = new TestClass2();
+        Method testMethod = TestClass2.class.getMethod("test1");
+        listener.beforeTestMethod(testObject, testMethod);
+        statement.evaluate();
+        EasyMock.expectLastCall().andThrow(new NullPointerException());
+
+        EasyMockUnitils.replay();
+        new BeforeTestMethodStatement(listener, statement, testMethod, testObject).evaluate();
+
+    }
+
+    @Test
+    public void testEvaluateOk() throws Throwable {
+        TestClass2 testObject = new TestClass2();
+        Method testMethod = TestClass2.class.getMethod("test1");
+        listener.beforeTestMethod(testObject, testMethod);
+        statement.evaluate();
+
+        EasyMockUnitils.replay();
+        new BeforeTestMethodStatement(listener, statement, testMethod, testObject).evaluate();
+
+    }
+
+
+    private class TestClass2 {
+        @Test
+        public void test1() {
+            Assert.assertTrue(true);
+        }
+    }
+
+}
