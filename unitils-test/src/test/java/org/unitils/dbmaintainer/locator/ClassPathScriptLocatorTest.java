dadda25e69905ff71cf15c44033173860762b170
willemijnw <willemijnw@5f2b72d7-f913-0410-b7ee-f4ba798c8f3b>
2014-09-19 13:28:29 +0000

    I have changed the file seperator

--- unitils-test/src/test/java/org/unitils/dbmaintainer/locator/ClassPathScriptLocatorTest.java
+++ unitils-test/src/test/java/org/unitils/dbmaintainer/locator/ClassPathScriptLocatorTest.java
@@ -3,6 +3,7 @@ package org.unitils.dbmaintainer.locator;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import org.easymock.EasyMock;
 import org.junit.Assert;
@@ -10,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.unitils.UnitilsJUnit4TestClassRunner;
+import org.unitils.core.Unitils;
 import org.unitils.dbmaintainer.locator.resourcepickingstrategie.ResourcePickingStrategie;
 import org.unitils.dbmaintainer.script.Script;
 import org.unitils.easymock.EasyMockUnitils;
@@ -32,6 +34,8 @@ public class ClassPathScriptLocatorTest {
 
     @Mock
     private ResourcePickingStrategie resourcePickingStrategie;
+    
+    private Properties configuration;
 
 
     private List<URL> urlList;
@@ -49,6 +53,7 @@ public class ClassPathScriptLocatorTest {
         urlList.add(getClass().getResource(scriptDir + "002_Initial_TESTcreate.sql"));
         urlList.add(getClass().getResource(scriptDir + "003_Initial_TESTcreate.sql"));
         Assert.assertFalse("Some of the needed resources is not found", urlList.contains(null));
+        configuration = Unitils.getInstance().getConfiguration();
     }
 
     /*** */
@@ -65,7 +70,7 @@ public class ClassPathScriptLocatorTest {
 
         EasyMockUnitils.replay();
         classPathScriptLocator = new ClassPathScriptLocator();
-        classPathScriptLocator.loadScripts(scriptList, path, resourcePickingStrategie, scriptExtensions, "users", true);
+        classPathScriptLocator.loadScripts(scriptList, path, resourcePickingStrategie, scriptExtensions, "users", true, configuration);
 
 
         Assert.assertTrue(classPathScriptLocator.scriptList.size() == 3);
